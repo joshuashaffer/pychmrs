e01e066267d1cfedece90334d6af99d3d92db080
jbs <joshua.shaffer@icmrl.net>
2026-06-06 22:24:05 -0300

    README.md, prefix and entry options.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Pychmrs: PyO3 Wrapper for the libchm (rust) Library
+
+## Why?
+
+All the churn in the Python build ecosystem broke most of the existing chm reading libraries, and it was easy enough to
+make this wrapper around this rust library.
+
+## Overview
+```python
+# Example usage of the PyChmRS library
+import pychmrs
+chm_file  = pychmrs.ChmFileWrapper('cool.chm')
+# [... , '/020161622x_cnode1.html',... ]
+byte_string = chm_file.read('/020161622x_cnode1.html')
+
+```
+
+## Installation
+```shell
+uv venv 
+uv pip install .
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,130 @@
 use pyo3::prelude::*;
 
-use libchm::{ChmFile, EntrySel};
-use std::ops::{Deref, DerefMut};
+#[pymodule]
+pub mod pychmrs {
+    use std::ops::{
+        Deref,
+        DerefMut,
+    };
 
-#[pyclass]
-struct ChmFileWrapper {
-    inner: ChmFile,
-}
+    use libchm::{
+        ChmFile,
+        EntrySel,
+    };
+    use pyo3::prelude::*;
 
-impl Deref for ChmFileWrapper {
-    type Target = ChmFile;
-
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
-}
-
-impl DerefMut for ChmFileWrapper {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
-    }
-}
-
-#[pymethods]
-impl ChmFileWrapper {
-    #[new]
-    pub fn __new__(obj: String) -> PyResult<Self> {
-        Ok(Self {
-            inner: ChmFile::open(obj).map_err(|_e| {
-                PyErr::new::<pyo3::exceptions::PyIOError, _>("failed to open chm file")
-            })?,
-        })
+    #[pyclass(from_py_object, eq, eq_int)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    enum PyEntrySel {
+        /// Paths starting with `/` but not `/#` or `/$`.
+        Normal  = 0x01,
+        /// Paths starting with `/#` or `/$`.
+        Special = 0x02,
+        /// Paths not starting with `/` (internal metadata).
+        Meta    = 0x04,
+        /// Non-directory entries (path does not end with `/`).
+        Files   = 0x08,
+        /// Directory entries (path ends with `/`).
+        Dirs    = 0x10,
+        /// All entries.
+        All     = 0x1F,
     }
 
-    pub fn read(&mut self, path: &str) -> PyResult<Vec<u8>> {
-        let inner: &mut ChmFile = self;
-        let entry = inner
-            .find(path)
-            .map_err(|_e| PyErr::new::<pyo3::exceptions::PyKeyError, _>("entry not found"))?;
-        inner
-            .read(&entry)
-            .map_err(|_e| PyErr::new::<pyo3::exceptions::PyKeyError, _>("failed to read"))
+    impl From<PyEntrySel> for EntrySel {
+        fn from(val: PyEntrySel) -> Self {
+            match val {
+                PyEntrySel::Normal => EntrySel::NORMAL,
+                PyEntrySel::Special => EntrySel::SPECIAL,
+                PyEntrySel::Meta => EntrySel::META,
+                PyEntrySel::Files => EntrySel::FILES,
+                PyEntrySel::Dirs => EntrySel::DIRS,
+                PyEntrySel::All => EntrySel::ALL,
+            }
+        }
     }
 
-    pub fn list_paths(&mut self) -> PyResult<Vec<String>> {
-        let inner: &mut ChmFile = self;
-        let entries = inner
-            .entries(EntrySel::all())
-            .map_err(|_e| PyErr::new::<pyo3::exceptions::PyIOError, _>("entry not found"))?;
-        Ok(entries.iter().map(|e| e.path.clone()).collect())
+    #[pyclass]
+    struct ChmFileWrapper {
+        inner: ChmFile,
+    }
+
+    impl Deref for ChmFileWrapper {
+        type Target = ChmFile;
+
+        fn deref(&self) -> &Self::Target {
+            &self.inner
+        }
+    }
+
+    impl DerefMut for ChmFileWrapper {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.inner
+        }
+    }
+
+    #[pymethods]
+    impl ChmFileWrapper {
+        #[new]
+        pub fn __new__(obj: String) -> PyResult<Self> {
+            Ok(Self {
+                inner: ChmFile::open(obj).map_err(|_e| {
+                    PyErr::new::<pyo3::exceptions::PyIOError, _>(
+                        "failed to open chm file",
+                    )
+                })?,
+            })
+        }
+
+        pub fn read(
+            &mut self,
+            path: &str,
+        ) -> PyResult<Vec<u8>> {
+            let inner: &mut ChmFile = self;
+            let entry = inner
+                .find(path)
+                .map_err(|_e| {
+                    PyErr::new::<pyo3::exceptions::PyKeyError, _>(
+                        "entry not found",
+                    )
+                })?;
+            inner
+                .read(&entry)
+                .map_err(|_e| {
+                    PyErr::new::<pyo3::exceptions::PyIOError, _>(
+                        "failed to read",
+                    )
+                })
+        }
+
+        #[pyo3(signature = (prefix = None, entry_sel = PyEntrySel::All))]
+        #[pyo3(text_signature = "(self, prefix: str = None, \
+                                 entry_sel: PyEntrySel = \
+                                 PyEntrySel.All)")]
+        pub fn list_paths(
+            &mut self,
+            prefix: Option<String>,
+            entry_sel: PyEntrySel,
+        ) -> PyResult<Vec<String>> {
+            let inner: &mut ChmFile = self;
+
+            let entries = if let Some(p) = prefix {
+                inner.entries_in(p.as_str(), entry_sel.into())
+            } else {
+                inner.entries(entry_sel.into())
+            };
+
+            let entries = entries.map_err(|_e| {
+                PyErr::new::<pyo3::exceptions::PyIOError, _>(
+                    "entry not found",
+                )
+            })?;
+            Ok(entries
+                .iter()
+                .map(|e| {
+                    e.path
+                        .clone()
+                })
+                .collect())
+        }
     }
 }
